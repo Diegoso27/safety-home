3bee2cf1e71c7dd9352b48ed0a23aa5a47397ab1
Diego.aml19@gmail.com <diego.aml19@gmail.com>
2024-07-06 21:29:20 -0400

    push2

--- src/app/pages/auth/forgot-password/forgot-password.page.ts
+++ src/app/pages/auth/forgot-password/forgot-password.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-forgot-password',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordPage implements OnInit {
 
+  firebaseService = inject(FirebaseService);
+  utilsService = inject(UtilsService)
+
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
+
   constructor() { }
 
   ngOnInit() {
   }
+
+
+  async submit() {
+    if (this.form.valid) {
+      const loading = await this.utilsService.loading();
+      await loading.present();
+
+      this.firebaseService.sendRecoveryEmail(this.form.value.email)
+        .then(resp => {
+          this.utilsService.presentToast({
+            message: 'Se ha enviado un correo para recuperar su contraseña',
+            duration: 1500,
+            color: 'primary',
+            position: 'bottom',
+            icon: 'mail-outline'
+          })
+
+          this.utilsService.routerLink('/auth');
+          this.form.reset();
+        }).catch(error => {
+          console.log(error);
+          this.utilsService.presentToast({
+            message: error.message,
+            duration: 1500,
+            color: 'danger',
+            position: 'bottom',
+            icon: 'alert-circle-outline'
+          })
+        }).finally(() =>{
+          loading.dismiss();
+        })
+    }
+  }
+
 
 }
