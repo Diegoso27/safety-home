49c3bef17186ba70a6d3fdf924c9d4bbcfa03a00
JavierSalamanca <javier.s.caceres@gmail.com>
2024-07-08 04:37:26 -0400

    feat(notification): create notification service and (create build and move google-service.json to android/app/)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'safetyhome',
-  webDir: 'www'
+  appId: 'com.app.android',
+  appName: 'safetyHome',
+  webDir: 'www',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
+  },
 };
 
 export default config;
